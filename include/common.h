750de4b1f0f4c08e6b174e32c55d626170415653
Amadeus <amadeus_osa@yahoo.fr>
2014-03-18 23:43:00 +0100

    Réorganisation du dépôt (2)

--- include/common.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef enum {false, true} bool;
--- include/videoutils.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-bool get_video_fps(const char* filename, int* fps);
-bool extract_images(const char* filename, int interval, int fps, const char* imagefolder);
